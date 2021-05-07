d60d07cfcf5d97273069301491f00807ded289f1
irakoze-hortance <hortanceirakoze@gmail.com>
2021-05-07 05:40:34 -0700

    new

---
+++ models/card.model.js
@@ -0,0 +1,41 @@
+const mongoose=require('mongoose')
+const Joi=require('joi')
+const Schema=mongoose.Schema
+
+const Card= new Schema({
+    card_id:{
+        type: String,
+        required: true
+    },
+    card_owner:{
+        type:String,
+        required:true
+    },
+    amount:{
+        type:Number,
+        required:true
+    },
+    fare:{
+        type:Number,
+        default:250,
+    },
+    final_Amount:{
+        type:Number,
+        default:amount-250,
+    }
+
+})
+
+function  validateCard(Card){
+    const JoiSchema=Joi.object({
+        card_id:Joi.string(),
+        card_owner:Joi.string(),
+        amount:Joi.number(),
+        fare:Joi.number(),
+        final_Amount:Joi.number(),
+    }).options({abortEarly:false});
+    return JoiSchema.validate(Card)
+
+}
+module.exports.Card= mongoose.model('Card',Card)
+module.exports.validateCard=validateCard
