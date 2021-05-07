79fd73fcfc72971ad4af15e26186585bd316c422
irakoze-hortance <hortanceirakoze@gmail.com>
2021-05-07 05:12:04 -0700

    initial commit

--- database/model.py
+++
@@ -1,7 +0,0 @@
-from .db import db
-
-
-class Card(db.Document):
-    card_id = db.StringField(required=True, unique=True)
-    uu_id = db.IntField(required=True)
-    amount = db.IntField(required=True)
