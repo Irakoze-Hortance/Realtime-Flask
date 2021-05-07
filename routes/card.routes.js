a1f805ab8b4eaf61435eaec164de56f326d803e8
irakoze-hortance <hortanceirakoze@gmail.com>
2021-05-07 06:21:45 -0700

    message

--- routes/card.routes.js
+++ routes/card.routes.js
@@ -2,7 +2,14 @@ const express=require('express')
 const cardController=require('../controllers/card.controller')
 const router=express.Router()
 
+
 router.post('/add',cardController.addCard)
 router.get('/all',cardController.getCards)
+
+router.get('/all_cards', async function(req, res) {
+    let data = await Card.find();
+    console.log("cards collection: ", data);
+    res.render('../views/card.view',{cardDetails : data})
+});
 
 module.exports=router
