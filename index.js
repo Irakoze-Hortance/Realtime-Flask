509eae3e0cfacb25d357b9d2758e8ebf3f0c28b7
irakoze-hortance <hortanceirakoze@gmail.com>
2021-05-07 07:02:51 -0700

    initial commit

--- index.js
+++ index.js
@@ -6,11 +6,19 @@ require('./database/db')
 const app=express()
 const port=8080
 const cardRouter=require('./routes/card.routes')
+
 app.set('view engine', 'ejs');
+app.set('view options', {layout: false});
+
 
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
 app.use(cors())
+
+
+app.get('/',(req,res) => {
+    res.render('card',{cardDetails : {name:"mukunzi"},age:23})
+})
 
 app.use('/api/',cardRouter)
 app.listen(port,()=>{
