dff83670e8dd76b6d3c5cbc77c025e1df981e295
irakoze-hortance <hortanceirakoze@gmail.com>
2021-05-07 05:31:51 -0700

    commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const bodyParser=require('body-parser')
+const cors = require('cors')
+
+require('./database/db')
+const app=express()
+const port=8080
+const cardRouter=require('./routes/card.routes')
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.use('/api/',cardRouter)
+app.listen(port,()=>{
+    console.log(`server running on port ${port}`)
+})
